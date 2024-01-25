f0f7f69544d5ebdbe4522b9a604b36f733d91d64
Hunter <cazadorcafe@gmail.com>
2024-01-24 19:55:12 -0700

    starter

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white p-4 mt-4 mr-2 ml-2">
+      <div className="container mx-auto">
+        <h1 className="text-2xl font-bold text-green-700">Recipe</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
